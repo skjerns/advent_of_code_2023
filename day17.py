21ccb8b20deeab0e953f8196622543d4f5a64027
Simon Kern <14980558+skjerns@users.noreply.github.com>
2023-12-22 22:34:31 +0100

    add my day 17 solution that should work but doesnt. i accidentially just guessed the right offset :-/ but can't figure out why.

--- day17.py
+++ day17.py
@@ -11,7 +11,7 @@ from tqdm import tqdm
 from itertools import product, combinations_with_replacement
 import networkx
 import heapq
-
+import psutil
 
 test_input = """2413432311323
 3215453535623
@@ -33,107 +33,73 @@ c = test_input
 #     c = f.read().strip()
 
 
-
 #%% failed dijkstra apporach
-
 class Node():
 
+    counterdirection = {0:1, 1:0, 2:3, 3:2}
+    direction2d = dict(enumerate([[-1, 0], [1, 0], [0, -1], [0, 1]]))
+    num2vec = {0:'^', 1:'v', 2:'<', 3:'>', -1:'o'}
 
     def __eq__(self, other):
-        if other is None: return False
-        return self.r == other.r  and self.c==other.c and \
-            self.prev_directions==other.prev_directions
+        return self.r == other.r and self.c==other.c and \
+            self.direction==other.direction and self.streak==other.streak
 
     def __hash__(self):
-        return hash(f'{self.r}, {self.c}, {self.prev_directions}')
+        return hash(self.r) + hash(self.c) + hash(self.streak) + hash(self.direction)
 
     def __repr__(self):
-        return f'Node({self.r}, {self.c}; heatloss={self.curr_heatloss} {self.prev_directions})'
+        return f'Node({self.r}, {self.c}; heatloss={self.curr_heatloss})'
 
-    @profile
     def __lt__(self, other):
         # return self.r>other.r and self.c>other.c
         if self.curr_heatloss<other.curr_heatloss:
             return True
-        elif self.curr_heatloss==other.curr_heatloss:
-            return self.r>other.r or self.c>other.c
+        # elif self.curr_heatloss==other.curr_heatloss:
+        #     return self.r>other.r or self.c>other.c
         return False
 
-    @profile
-    def __init__(self, r, c, grid, prev_node, path_cumulative_heatloss, direction=None):
+    def __init__(self, r, c, grid, direction, streak, heatloss, prev_node=None):
         self.r = r
         self.c = c
-        self.curr_heatloss = np.inf
-        self.prev_directions = []
         self.grid = grid
-        self.prev_node = prev_node
-        self.curr_heatloss = path_cumulative_heatloss+grid[r, c]
         self.direction = direction
-        self.neighbours = []
+        self.streak=streak
+        self.curr_heatloss = heatloss+grid[r, c]
+        self.prev_node = prev_node
+        # if streak>3:
+        #     self.curr_heatloss = np.inf
 
-        if prev_node is None:
-            self.prev_directions = []
-            return
-
-        if prev_node.prev_node is None:
-            self.prev_directions = [prev_node.direction]
-            return
-
-        if prev_node.prev_node.prev_node is None:
-            self.prev_directions =  [prev_node.prev_node.direction, prev_node.direction]
-            return
-
-        self.prev_directions =  [prev_node.prev_node.prev_node.direction,
-                                 prev_node.prev_node.direction,
-                                 prev_node.direction,
-                                 direction]
-
-        for x in self.prev_directions[:-1]:
-            if direction!=x:
-                return
-
-        self.curr_heatloss = np.inf
-
-
-    @profile
     def get_neighbours(self):
-        assert self.curr_heatloss < np.inf
+        # assert self.curr_heatloss < np.inf
         neighbours = set()
-        for r, d in zip([-1, 1], ['^', 'v']):
-            next_r = self.r+r
+        for d, rc in enumerate([[-1, 0], [1, 0], [0, -1], [0, 1]]):
+            next_r = self.r+rc[0]
+            next_c = self.c+rc[1]
             if next_r<0 or next_r>=self.grid.shape[0]:
                 continue
-            if self.prev_node and next_r==self.prev_node.r:
-                continue
-            next_node = Node(next_r, self.c, self.grid, self,
-                             self.curr_heatloss, direction=d)
-            neighbours.add(next_node)
-
-        for c, d in zip([-1, 1], ['<', '>']):
-            next_c = self.c+c
             if next_c<0 or next_c>=self.grid.shape[1]:
                 continue
-            if self.prev_node and next_c==self.prev_node.c:
+            if Node.counterdirection[d]==self.direction:
                 continue
-            next_node = Node(self.r, next_c, self.grid, self,
-                             self.curr_heatloss, direction=d)
-            # print(next_node)
+            new_streak = 1 if self.direction!=d else self.streak+1
+            if new_streak>3:
+                continue
+            next_node = Node(next_r, next_c, self.grid, direction=d,
+                             streak = new_streak,  heatloss=self.curr_heatloss,
+                             prev_node=self)
             neighbours.add(next_node)
-        # if len(self.neighbours)==4:
-        #     pass
-        # print(len(self.neighbours))
+
         return neighbours
 
 
 
 class Grid():
-
-    def __init__(self, grid):
+    def __init__(self, grid, heuristic=True):
         grid = np.array([[int(x) for x in row] for row in grid.strip().split('\n')])
         self.grid = grid
-        prev_node = None
-        path_cumulative_heatloss = -grid[0,0]
-        self.curr_node = Node(0, 0, grid, prev_node, path_cumulative_heatloss)
+        self.heuristic = heuristic
+        # path_cumulative_heatloss = -grid[0,0]
+        self.curr_node = Node(0, 0, grid, -1, -1, -grid[0,0])
         self.visited = set()
         # indices = product(range(grid.shape[0]), range(grid.shape[1]))
         self.not_visited = []
@@ -143,7 +109,6 @@ class Grid():
         heapq.heappush(self.not_visited, self.curr_node)
         # self.not_visited = [self.curr_node]
 
-    @profile
     def explore_neighbours(self):
         # visit lowest node
         try:
@@ -161,42 +126,69 @@ class Grid():
         for neighbour in neighbours:
             # if neighbour not in self.visited and not neighbour.curr_heatloss==np.inf:
                 # heapq.heappush(self.not_visited, neighbour)
-            if neighbour not in self.visited:
+            # heuristic to stop early
+            if self.heuristic:
                 manhattendistance = grid.shape[0]-neighbour.r + grid.shape[1]+neighbour.c
-                if neighbour.curr_heatloss + manhattendistance < self.upper_bound:
+            else:
+                manhattendistance = 0
+            if neighbour.curr_heatloss + manhattendistance < self.upper_bound and \
+               neighbour not in self.visited and not neighbour in self.not_visited:
+                # manhattendistance = grid.shape[0]-neighbour.r + grid.shape[1]+neighbour.c
+                # if neighbour.curr_heatloss + manhattendistance < self.upper_bound:
                 # if not neighbour.curr_heatloss==np.inf:
-                    heapq.heappush(self.not_visited, neighbour)
+                heapq.heappush(self.not_visited, neighbour)
 
-                # self.not_visited.append(neighbour)
-                # idx = self.not_visited.index(neighbour)
-
-        # self.not_visited = sorted(self.not_visited)
         return True
 
-    def __repr__(self):
-        plot_path(self.path[0], self.grid)
 
     def plot_heatmap(self):
         plt.cla()
+        grid = self.grid
         heatmap= np.zeros(self.grid.shape)
         # for node in self.not_visited:
         #     heatmap[node.r, node.c] = node.curr_heatloss
+        ax = plt.gca()
+
         for node in self.visited:
             heatmap[node.r, node.c] = node.curr_heatloss
+
         vmax = self.grid[np.eye(*self.grid.shape, dtype=bool)].sum()
-        ax = plt.gca()
-        ax.imshow(heatmap.T, vmax=vmax, origin='upper')
+        ax.imshow(heatmap, vmax=vmax, origin='upper')
+        # ax.imshow(grid, alpha=0.2)
+        if grid.shape[0]<20:
+            plt.xticks(np.arange(grid.shape[1]))
+            plt.yticks(np.arange(grid.shape[0]))
+            plt.tick_params(labeltop=True, labelright=True)
+
+            for node in self.visited:
+                text = ax.text(node.c, node.r, node.curr_heatloss,
+                               fontsize=14, ha="center", va="center",
+                               color="green")
+
+            for r, row in enumerate(grid):
+                for c, val in enumerate(row):
+                    text = ax.text(c, r-0.25, val,
+                                   fontsize=14, ha="center", va="center",
+                                   color="gray")
+
+        for node in self.not_visited:
+            highlight_cell(node.c, node.r, ax=ax, color='red')
+            if grid.shape[0]<20:
+                text = ax.text(node.c, node.r, node.curr_heatloss,
+                               fontsize=14, ha="center", va="center",
+                               color="black")
 
         node = self.curr_node
-        highlight_cell(node.r, node.c, ax=plt.gca())
-        text = ax.text(node.c, node.r, node.direction,
-                    fontsize=14,
-                       ha="center", va="center", color="black")
+        for nodex in self.visited:
+            if nodex.r==grid.shape[0]-1 and nodex.c==grid.shape[1]-1:
+                node = nodex
+        highlight_cell(node.c, node.r, ax=ax, color='blue')
+
         while (node:=node.prev_node) is not None:
             highlight_cell(node.c, node.r, ax=ax)
-            text = ax.text(node.c, node.r, node.direction,
+            text = ax.text(node.c, node.r+0.25, Node.num2vec[node.direction],
                     fontsize=14,
-                       ha="center", va="center", color="black")
+                        ha="center", va="center", color="black")
 
 
 def highlight_cell(x,y, ax=None, **kwargs):
@@ -204,16 +196,6 @@ def highlight_cell(x,y, ax=None, **kwargs):
     ax = ax or plt.gca()
     ax.add_patch(rect)
     return rect
-
-def plot_path(node, grid):
-    grid_str = grid.copy().astype(str)
-
-    grid_str[node.r, node.c] = node.direction
-
-    while (node:=node.prev_node)not in (BaseNode(0, 0), None):
-        if node is not None and node.direction is not None:
-            grid_str[node.r, node.c] = node.direction
-    print('\n'.join([''.join([x for x in row]) for row in grid_str]))
 
 
 grid = c
@@ -224,19 +206,23 @@ grid = self.grid
 loop = tqdm()
 i = 0
 while self.explore_neighbours():
+    break
     loop.update()
     i+=1
-    # if i==5000:
+    # if i==50000:
         # break
-    # # plot_path(self.visited[-1], grid)
-    # if i%1000000==0:
-        # self.plot_heatmap()
-        # plt.pause(0.01)
+    # plot_path(self.visited[-1], grid)
+    if i%10000==0:
+        self.plot_heatmap()
+        plt.pause(0.01)
+        if psutil.virtual_memory().percent>85:
+            raise Exception('outofmemory')
     # print(len(curr_node))
     # input()
 
-heatlosses = [node.curr_heatloss for node in self.visited if node.r==12 and node.c==12]
-print(f'{min(heatlosses)=}  {heatlosses=}')
+heatlosses = [node.curr_heatloss for node in self.visited if node.r==grid.shape[0]-1 and node.c==grid.shape[1]-1]
+if len(heatlosses)>0:
+    print(f'{min(heatlosses)=}  {heatlosses=}')
 
 # for node in self.visited:
 #     if node.r==12 and node.c==12:
@@ -246,3 +232,81 @@ print(f'{min(heatlosses)=}  {heatlosses=}')
 #         plt.waitforbuttonpress(50)
 
 # 882 too high
+
+
+#%% part 2
+
+
+def get_neighbours_ultra(self):
+    # assert self.curr_heatloss < np.inf
+    neighbours = set()
+
+    for d, (r, c) in enumerate([[-1, 0], [1, 0], [0, -1], [0, 1]]):
+        # if we are turning, move at least three steps
+        # however, also add the heatloss of the path we have taken
+        heatloss_path = 0
+        if d!=self.direction:
+            r, c = [r*4, c*4]
+            new_streak = 4
+        else:
+            new_streak = 1 if self.direction!=d else self.streak+1
+        next_r = self.r + r
+        next_c = self.c + c
+
+        if next_r<0 or next_r>=self.grid.shape[0]:
+            continue
+        if next_c<0 or next_c>=self.grid.shape[1]:
+            continue
+        if Node.counterdirection[d]==self.direction:
+            continue
+
+        if d!=self.direction:
+            # try:
+                heatloss_path += sum([grid[self.r+(r>0)*i, self.c+(c>0)*i] for i in range(1,4)])
+            # except Exception:
+                # pass
+        # no more than 10 moves in one direction
+        if new_streak>9:
+            continue
+        next_node = Node(next_r, next_c, self.grid, direction=d,
+                         streak = new_streak,  heatloss=self.curr_heatloss + heatloss_path,
+                         prev_node=self)
+        neighbours.add(next_node)
+    return neighbours
+
+Node.get_neighbours = get_neighbours_ultra
+
+grid = c
+# grid = """111111111111
+# 999999999991
+# 999999999991
+# 999999999991
+# 999999999991"""
+
+self = Grid(grid, heuristic=False)
+grid = self.grid
+
+loop = tqdm()
+i = 0
+
+while self.explore_neighbours():
+    loop.update()
+    i+=1
+    # if i==50000:
+    #     # break
+    # # plot_path(self.visited[-1], grid)
+
+    if i%10000==0:
+        self.plot_heatmap()
+        plt.pause(0.01)
+    # plt.waitforbuttonpress(30)
+
+    # print(len(curr_node))
+    # input()
+
+heatlosses = [node.curr_heatloss for node in self.visited if node.r==grid.shape[0]-1 and node.c==grid.shape[1]-1]
+print(f'{min(heatlosses)=}  {heatlosses=}')
+
+# 720 too low
+# why is it 734????
+# my solution shows 743.... :-/
